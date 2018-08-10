118771aa77e05eac82ee012d003af554d99d0724
Ubuntu <ubuntu@ip-172-31-14-9.us-east-2.compute.internal>
2018-08-10 17:31:41 +0000

    added triangle

--- triangleTrader/controller.py
+++ triangleTrader/controller.py
@@ -9,9 +9,9 @@ __maintainer__ = "Lutz Künneke"
 __email__ = "lutz.kuenneke89@gmail.com"
 __status__ = "Prototype"
 """
-Spread trading using UK/US 10yr bond yields
+Places orders to trade the triangle
 Use at own risk
-Author: Lutz Kuenneke, 26.07.2018
+Author: Lutz Kuenneke, 12.07.2018
 """
 import json
 import time
@@ -22,7 +22,6 @@ import configparser
 import math
 import datetime
 import numpy as np
-import pandas as pd
 
 
 class controller(object):
@@ -38,6 +37,8 @@ class controller(object):
                 'active_account')
         self.settings['v20_host'] = config.get('demo', 'hostname')
         self.settings['v20_port'] = config.get('demo', 'port')
+        self.settings['risk_factor'] = int(config.get('triangle', 'risk_factor'))
+        self.settings['account_risk'] = int(config.get('triangle', 'account_risk'))
         self.oanda = v20.Context(self.settings.get('v20_host'),
                                  port=self.settings.get('v20_port'),
                                  token=self.settings.get('access_token'
@@ -48,25 +49,6 @@ class controller(object):
         self.trades = \
             self.oanda.trade.list_open(self.settings.get('account_id'
                 )).get('trades', '200')
-        self.dataObj = {
-        #'uk10y': { 'name': 'UK10YB_GBP' },
-        #'us10y': { 'name':  'USB10Y_USD'},
-        #'uk100': { 'name':  'UK100GBP'},
-        #'us30': { 'name': 'US30_USD'},
-        #'gu': { 'name': 'GBP_USD'}
-        'wheat': {'name': 'WHEAT_USD' },
-        'corn': { 'name': 'CORN_USD' },
-        'sugar': { 'name': 'SUGAR_USD' }}
-    def retrieveData(self):
-        for key in self.dataObj.keys():
-            self.dataObj[key]['candles'] = self.getCandles(self.dataObj[key].get('name'),'H4',500)
-    def writeData(self, fname):
-        writeObj = {}
-        for key in self.dataObj.keys():
-            colname = self.dataObj[key].get('name')
-            writeObj[colname] = [float(candle.get('mid').get('c')) for candle in self.dataObj[key]['candles']]
-        df = pd.DataFrame.from_dict(writeObj)
-        df.to_csv(fname)
     def getConversion(self, leadingCurr):
         # get conversion rate to account currency
         accountCurr = 'EUR'
@@ -124,7 +106,7 @@ class controller(object):
                  )[0].get('price'))) / 2.0
         return price
 
-    def getCandles(
+    def getTriangle(
         self,
         ins,
         granularity,
@@ -135,9 +117,97 @@ class controller(object):
                           )
         request.set_path_param('instrument', ins)
         request.set_path_param('count', numCandles)
-        request.set_path_param('price', 'M')
+        request.set_path_param('price', 'MBA')
         request.set_path_param('granularity', granularity)
         response = self.oanda.request(request)
-        print(response.raw_body)
-        candles = json.loads(response.raw_body)
-        return candles.get('candles')
+        candles = json.loads(response.raw_body).get('candles')
+        upperFractals = []
+        lowerFractals = []
+        fractRange = 2
+        for i in range(fractRange,len(candles)-fractRange):
+         isupper = True
+         islower = True
+         for k in range(i-fractRange,i+fractRange):
+          if i == k:
+           continue
+          if float(candles[i].get('ask').get('h')) < float(candles[k].get('ask').get('h')):
+           isupper = False
+          if float(candles[i].get('bid').get('l')) > float(candles[k].get('bid').get('l')):
+           islower = False
+         if isupper:
+          upperFractals.append(candles[i])
+         if islower:
+          lowerFractals.append(candles[i])
+        return sma
+
+    def checkIns(self, ins):
+        price = self.getPrice(ins)
+
+        # get the ducks
+
+        smas = [self.getSMA(ins, duck.get('granularity'),
+                duck.get('numCandles')) for duck in self.ducks]
+
+        # first ducks must be aligned, third one in the making for us to place an order
+
+        bullish = [sma < price for sma in smas] == [True, True, False]
+        bearish = [sma > price for sma in smas] == [True, True, False]
+        if bullish:
+            print(ins + ' is bullish')
+            direction = 1
+        if bearish:
+            print(ins + ' is bearish')
+            direction = -1
+        if not bullish and not bearish:
+            print(ins + ' no clear signal, dont touch')
+            return
+
+        # check whether there is an open trade
+
+        pipLoc = self.getPipSize(ins)
+        pipVal = 10 ** (-pipLoc + 1)
+        entry = smas[2]  # use third ducks sma as entr
+        sl = smas[1] # take scnd duck as stop
+        sldist = abs(price - sl)
+        tp = entry + (entry - sl)/0.618 # some serious FIB stuff
+        if abs(sl-entry)/pipVal < self.settings.get('stopLoss'):
+            tp = entry + direction * pipVal * self.settings.get('takeProfit')
+            sl = entry - direction * pipVal * self.settings.get('stopLoss')
+            sldist = abs(entry - sl)
+        units = direction * self.settings.get('units')
+
+        # round off for the v20 api to accept the stops
+
+        units = self.getUnits(abs(sl-entry),ins) * direction
+        if abs(units) < 1:
+            return
+        currentUnits = np.sum([float(trade.currentUnits) for trade in self.trades if trade.instrument == ins])
+        if currentUnits * units > 0 and abs(units)< 2*abs(currentUnits):
+            print('Skipping ' + ins)
+            return
+        if units*currentUnits> 0 and currentUnits > 0:
+            units -= currentUnits
+
+        fstr = '30.' + str(pipLoc) + 'f'
+        tp = format(tp, fstr).strip()
+        sl = format(sl, fstr).strip()
+        sldist = format(sldist, fstr).strip()
+        entry = format(entry, fstr).strip()
+        expiry = datetime.datetime.now() + datetime.timedelta(hours=1)
+        args = {'order': {
+            'instrument': ins,
+            'units': units,
+            'price': entry,
+            'type': 'STOP',
+            'timeInForce': 'GTD',
+            'gtdTime': expiry.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+            'takeProfitOnFill': {'price': tp, 'timeInForce': 'GTC'},
+            'trailingStopLossOnFill': {'distance': sldist, 'timeInForce': 'GTC'},
+            'stopLossOnFill': {'price': sl, 'timeInForce': 'GTC'},
+            }}
+        ticket = self.oanda.order.create(self.settings.get('account_id'
+                ), **args)
+        ticket_json = json.loads(ticket.raw_body)
+
+
+        print(ticket_json)
