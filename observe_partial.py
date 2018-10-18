3d9140e330fb02870950a3052108f750a65b7a83
LutzKuen <lutz.kuenneke89@gmail.com>
2018-10-18 14:28:12 +0000

    Added spread deduction from TP

--- observe_partial.py
+++ observe_partial.py
@@ -3,6 +3,6 @@ cont = controller.controller('/home/ubuntu/settings_triangle.conf','live')
 cont.retrieveData(4, completed = False, upsert = True)
 cont.data2sheet(complete = False)
 allowed_ins = [ins.name for ins in cont.allowed_ins]
-#returns = [cont.openLimit(ins) for ins in allowed_ins]
+returns = [cont.openLimit(ins, close_only = True, complete = False) for ins in allowed_ins]
 cont_demo = controller.controller('/home/ubuntu/settings_triangle.conf','demo')
 returns = [cont_demo.openLimit(ins, close_only = True, complete = False) for ins in allowed_ins]
