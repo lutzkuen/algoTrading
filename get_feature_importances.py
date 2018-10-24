99fd5ca69029ff3943c70f91adc9b62732e5c0af
LutzKuenneke <lutz.kuenneke89@gmail.com>
2018-10-24 22:20:17 +0200

    Release commit

--- get_feature_importances.py
+++ get_feature_importances.py
@@ -1,3 +1,7 @@
-from observer import controller_cython as controller
-cont = controller.Controller('/home/ubuntu/settings_triangle.conf',None)
+try:
+    from observer import controller_cython as controller
+except ImportError:
+    print('WARNING: Cython module not found, falling back to native python')
+    from observer import controller as controller
+cont = controller.Controller('../settings_triangle.conf', None)
 cont.get_feature_importances()
