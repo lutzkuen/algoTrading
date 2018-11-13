dd29ffcc7905139e9d7a2d582fc968034025d174
LutzKuen <lutz.kuenneke89@gmail.com>
2018-11-13 07:45:25 +0000

    add transaction cost check

--- functional_test.py
+++ functional_test.py
@@ -4,7 +4,7 @@ cont = controller.Controller('/home/ubuntu/settings_triangle.conf','live')
 cont.retrieve_data(1, upsert = True)
 cont.retrieve_data(1,completed = False, upsert = True)
 cont.data2sheet()
-#allowed_ins = [ins.name for ins in cont.allowed_ins]
-#cont_demo = controller.Controller('/home/ubuntu/settings_triangle.conf','demo')
-#returns = [cont_demo.open_limit(ins) for ins in allowed_ins]
-#returns = [cont_demo.open_limit(ins, close_only = True, complete = False) for ins in allowed_ins]
+allowed_ins = [ins.name for ins in cont.allowed_ins]
+cont_demo = controller.Controller('/home/ubuntu/settings_triangle.conf','demo')
+returns = [cont_demo.open_limit(ins) for ins in allowed_ins]
+returns = [cont_demo.open_limit(ins, close_only = True, complete = False) for ins in allowed_ins]
