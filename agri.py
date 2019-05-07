a48adbb6e7540c5e5466b44b5fcc30565fcaa8e4
lutzkuen <lutz.kuenneke89@gmail.com>
2019-05-07 07:53:11 +0000

    removed weird files

--- agri.py
+++
@@ -1,4 +0,0 @@
-#from agriculture import controller_cython as controller
-from agricultural import controller
-cont_demo = controller.Controller('/home/ubuntu/settings_triangle.conf','demo')
-cont_demo.open_orders()
--- staging/pipeiner.py
+++
@@ -1,5 +0,0 @@
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.feature_selection import SelectPercentile
-from sklearn.pipeline import make_pipeline
-
-
